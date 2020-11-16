572ca398a52ace38adc5c9abb1096bce33a86e67
Jamine Mueller <jamine.mueller@tenmedia.de>
2020-11-16 08:05:25 +0100

    l

--- resources/editorjs/types/configs/fixed-footer-block-config.d.ts
+++
@@ -1,6 +0,0 @@
-export interface FixedFooterBlockConfig {
-    /**
-     * This will be used as placeholder for footer
-     */
-    //placeholder: string;
-}
--- resources/editorjs/types/configs/fixed-title-block-config.d.ts
+++
@@ -1,6 +0,0 @@
-export interface FixedTitleBlockConfig {
-    /**
-     * This will be used as placeholder for title
-     */
-    //placeholder: string;
-}
